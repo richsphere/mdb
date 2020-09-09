38376d6d3cb7654fea2b1989955f93a944c6338b
Sergei Ponomarev <richsphere@gmail.com>
2020-09-09 18:31:51 +0500

    First commit

---
+++ django/core/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+app_name = 'core'
+urlpatterns = [
+    path('movies',
+         views.MovieList.as_view(),
+         name='MovieList'),
+    path('movie/<int:pk>',
+         views.MovieDetail.as_view(),
+         name='MovieDetail'),
+    path('movie/<int:movie_id>/vote',
+         views.CreateVote.as_view(),
+         name='CreateVote'),
+    path('movie/<int:movie_id>/vote/<int:pk>',
+         views.UpdateVote.as_view(),
+         name='UpdateVote'),
+]
